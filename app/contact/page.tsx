626396728b7e64570bde9fdbd68adaecbaa8c928
rrrachelreed-art <rr.rachelreed@gmail.com>
2025-09-03 09:16:59 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,10 @@
-import { ReactNode } from "react";
-
-export default function PageTemplate() {
+export default function Contact() {
   return (
     <div className="container mx-auto px-4 py-10">
-      <h1 className="text-3xl font-bold mb-6 text-rose-500">Page Title</h1>
-      <p>Nothing available yet</p>
+      <h1 className="text-3xl font-bold mb-6 text-rose-500">Contact</h1>
+      <p>📞 +356 79989843</p>
+      <p>📧 rr.rachelreed@gmail.com</p>
+      <p>📍 Malta</p>
     </div>
   );
 }
