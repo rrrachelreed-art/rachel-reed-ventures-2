250d6c3c31c7e4145b1c0f1318ab786ceffa8f45
rrrachelreed-art <rr.rachelreed@gmail.com>
2025-09-03 09:08:37 +0200

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Rachel Reed Ventures</title>
+      <meta name="description" content="Freelance Professional | Real Estate • Beauty • Lifestyle • Design" />
+    </>
+  );
+}
