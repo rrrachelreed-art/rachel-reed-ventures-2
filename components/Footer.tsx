0544f5a46d9d4238ece40b5ea6a09f17c80f67e0
rrrachelreed-art <rr.rachelreed@gmail.com>
2025-09-03 09:49:43 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-
 export default function Footer() {
   return (
-    <footer className="p-6 border-t border-gray-200 text-center">
-      <p>Rachel Reed | +356 79989843 | rr.rachelreed@gmail.com</p>
-      <p>© 2025 Rachel Reed Ventures</p>
+    <footer className="bg-white shadow-md py-4 mt-8">
+      <div className="container mx-auto text-center text-sm text-gray-500">
+        © {new Date().getFullYear()} Rachel Reed Ventures. All rights reserved.
+      </div>
     </footer>
   );
 }
