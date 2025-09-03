7b6e8335e20105bcb5520c90fb21b8a3a6cf81f1
rrrachelreed-art <rr.rachelreed@gmail.com>
2025-09-03 09:15:55 +0200

    Update page.tsx

--- app/cars/page.tsx
+++ app/cars/page.tsx
@@ -1,10 +1,8 @@
-import { ReactNode } from "react";
-
-export default function PageTemplate() {
+export default function Cars() {
   return (
     <div className="container mx-auto px-4 py-10">
-      <h1 className="text-3xl font-bold mb-6 text-rose-500">Page Title</h1>
-      <p>Nothing available yet</p>
+      <h1 className="text-3xl font-bold mb-6 text-rose-500">Cars</h1>
+      <p>Coming soon / Nothing yet</p>
     </div>
   );
 }
