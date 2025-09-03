b221f90975f2c8cb9bbf0f53d08b82278b961f0d
rrrachelreed-art <rr.rachelreed@gmail.com>
2025-09-03 08:58:42 +0200

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,1 +1,14 @@
+import React from "react";
 
+interface ButtonProps {
+  text: string;
+  link: string;
+}
+
+export default function Button({ text, link }: ButtonProps) {
+  return (
+    <a href={link}>
+      <button>{text}</button>
+    </a>
+  );
+}
