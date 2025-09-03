ab96de0ed1400689dc210ef74b5d1848a1769818
rrrachelreed-art <rr.rachelreed@gmail.com>
2025-09-03 08:59:04 +0200

    Update ListingCard.tsx

--- components/ListingCard.tsx
+++ components/ListingCard.tsx
@@ -1,1 +1,17 @@
+import React from "react";
 
+interface ListingCardProps {
+  title: string;
+  location: string;
+  price: number;
+}
+
+export default function ListingCard({ title, location, price }: ListingCardProps) {
+  return (
+    <div className="p-4 border border-gray-200 rounded-lg shadow hover:shadow-lg transition">
+      <h4 className="font-semibold mb-2">{title}</h4>
+      <p>{location}</p>
+      <p>€{price}</p>
+    </div>
+  );
+}
