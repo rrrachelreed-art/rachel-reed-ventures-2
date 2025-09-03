d4cf35f7d4f98c4dede36b0cf13917c95577795c
rrrachelreed-art <rr.rachelreed@gmail.com>
2025-09-03 08:59:21 +0200

    Update ServiceCard.tsx

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+interface ServiceCardProps {
+  title: string;
+  description: string;
+}
+
+export default function ServiceCard({ title, description }: ServiceCardProps) {
+  return (
+    <div className="p-4 border border-gray-200 rounded-lg shadow hover:shadow-lg transition">
+      <h4 className="text-lg font-semibold mb-2">{title}</h4>
+      <p>{description}</p>
+    </div>
+  );
+}
